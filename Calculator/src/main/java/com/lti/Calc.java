924dc1c8f965d35d28c303f8bf6e25847375c438
Devashish Gupta <devashish.gupta1998@gmail.com>
2020-12-21 15:10:04 +0530

    added prods

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -7,10 +7,14 @@ public class Calc {
 	public static int subs(int num1, int num2) {
 		return num1-num2;
 	}
+	public static int prod(int num1, int num2) {
+		return num1*num2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add is: "+add(10,20));
 		System.out.println("Substract is: "+subs(40,20));
+		System.out.println("prod is: "+prod(40,20));
 	}
 
 }
